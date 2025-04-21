346cdd09f8e044403a0e9ac8cae5e494d148e70c
setbr1 <dcdm2mil@gmail.com>
2025-04-20 23:13:53 -0300

    wip: css, mais configurações

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './global.css'
 import { Post } from './Post'
+import { Header } from './components/Header'
 
 export function App() {
 
   return (
     <div>
+      <Header/>
       <Post 
       author="JK Rowling" 
       content="Harry Potter"
