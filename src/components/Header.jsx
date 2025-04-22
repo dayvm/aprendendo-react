161ce6d3dcb7bd1ead66bcebd3fe4b1cc605d2bf
dayvm <83921667+dayvm@users.noreply.github.com>
2025-04-22 22:52:29 +0000

    wip: css em andamento

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import styles from './Header.module.css'
+import logo from '../assets/logo-random.webp'
 
 console.log(styles)
 
 export function Header () {
     return (
         <header className={styles.header}>
-            <strong>Ignite feed</strong>
+            <img src={logo} alt="logo do projeto" />
         </header>
     )
 }
